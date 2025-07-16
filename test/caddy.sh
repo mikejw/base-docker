4dd030a3f3a1afb036a2d60e7caeb10148ad93ec
Mike Whiting <mail@mikejw.co.uk>
2025-07-16 02:01:39 +0000

    working on test env

---
+++ test/caddy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+apt install -y debian-keyring debian-archive-keyring apt-transport-https curl
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | gpg  --batch --yes --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | tee /etc/apt/sources.list.d/caddy-stable.list
+chmod o+r /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+chmod o+r /etc/apt/sources.list.d/caddy-stable.list
+apt update
+apt install caddy
