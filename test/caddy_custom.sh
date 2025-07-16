c3ed1a9e5819b20e8e4c257b9cf7de5e82861543
Mike Whiting <mail@mikejw.co.uk>
2025-07-16 08:12:40 +0000

    working on deployments

--- test/caddy_custom.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-chmod +x ./caddy
-dpkg-divert --divert /usr/bin/caddy.default --rename /usr/bin/caddy
-mv ./caddy /usr/bin/caddy.custom
-update-alternatives --install /usr/bin/caddy caddy /usr/bin/caddy.default 10
-update-alternatives --install /usr/bin/caddy caddy /usr/bin/caddy.custom 50
-systemctl restart caddy
