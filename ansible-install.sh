fe5df5ce26606196a85c9f3ecf3eef2a9becf514
Mike Whiting <mikejw3@gmail.com>
2025-09-25 14:13:58 +0000

    caddy basic http auth

--- ansible-install.sh
+++ ansible-install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-ansible-galaxy role install -r requirements.yml --roles-path ./roles
-ansible-galaxy collection install -r requirements.yml -p ./collections --force
+ansible-galaxy role install -r requirements.yml
+ansible-galaxy collection install -r requirements.yml --force
